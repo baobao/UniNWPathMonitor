9369035c08614aebdd520641202e17a61f76fec7
baobao <shunsuke.ohba@gmail.com>
2020-08-21 06:29:31 +0900

    add Sample

---
+++ Assets/Sample/Sample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using info.shibuya24.Network;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample : MonoBehaviour
+{
+    [SerializeField] private Text _statusText;
+
+    void Start()
+    {
+        UniNWPathMonitor.StartMonitor();
+        UniNWPathMonitor.onChangeNetworkStatus += status =>
+            {
+                _statusText.text = status == UniNWPathMonitor.NetworkStatus.satisfied ? "接続中" : "切断中";
+            };
+    }
+}
